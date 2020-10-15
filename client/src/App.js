e3cba46e1d0f07462ab5d2342b30107d8be7a193
p00gz <bloopsocket@gmail.com>
2020-10-14 22:59:47 -0500

    test

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React 1
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
